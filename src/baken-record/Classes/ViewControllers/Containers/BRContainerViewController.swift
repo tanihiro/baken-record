6cda0d983063b237bddd43ab85504ae172811220
itani-hiroki <itanihiroki@gmail.com>
2015-09-27 21:41:11 +0900

    add container viewcontroller

---
+++ src/baken-record/Classes/ViewControllers/Containers/BRContainerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BRContainerViewController.swift
+//  baken-record
+//
+//  Created by 井谷 裕紀 on 2015/09/27.
+//  Copyright © 2015年 hiroki-itani. All rights reserved.
+//
+
+import UIKit
+
+class BRContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
